36dfef119ff64b7c56b4a761df2f96af2e5b8c67
michelkruger <micheldequadros@gmail.com>
2023-06-09 01:45:35 -0300

    update landing with contac, tecnologie and other section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,18 +5,23 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Header from '../../components/Header'
 import Logo from '../../assets/logo-brancov2.png'
-import AboutImage from '../../assets/12.png'
-import serviceImg from '../../assets/2.png'
-import serviceImg4 from '../../assets/6.png'
+import AboutImage from '../../assets/about.png'
+import serviceImg from '../../assets/3.png'
+import serviceImg4 from '../../assets/5.png'
 import serviceImg3 from '../../assets/4.png'
+import ReactIcon from '../../assets/react.svg'
+import NodeIcon from '../../assets/node.svg'
+import EvernoteIcon from '../../assets/evernote.svg'
+import Figma from '../../assets/figma.svg'
+import Github from '../../assets/github.svg'
+import Aws from '../../assets/aws.svg'
+
 import { Animated } from 'react-animated-css'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faComputerMouse,
-  faChevronDown,
-
-
+  faChevronDown
 } from '@fortawesome/free-solid-svg-icons'
 
 const Home = () => {
@@ -25,7 +30,10 @@ const Home = () => {
 
   if (typeof window !== 'undefined') {
     window.addEventListener('scroll', function () {
-      var scrollTop = window.pageYOffset || document.documentElement.scrollTop
+      let logoContainer = document.querySelector('.logo-container')
+
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+
       if (scrollTop > 350 && scrollTop < 700) {
         setShowAbout(true)
       }
@@ -33,17 +41,24 @@ const Home = () => {
       if (scrollTop > 900 && scrollTop < 1800) {
         setShowServices(true)
       }
+
+      if (scrollTop > 10) {
+        logoContainer.classList.add('logo-container-fixed')
+      } else {
+        logoContainer.classList.remove('logo-container-fixed')
+      }
+
       console.log('Posição vertical atual:', scrollTop)
     })
   }
 
   return (
     <main>
-      <header className="w-full pl-7 pr-7 pt-3 pb-6">
-        <div className="logo-container w-full">
+      <header className="w-full pb-6">
+        <div className="logo-container w-full pt-3 pb-3 pl-7">
           <Image src={Logo} width={210} alt="Logo - Nowne" />
         </div>
-        <section className="sectionContainer w-full h-screen content-center">
+        <section className="sectionContainer w-full h-screen content-center pl-7 pr-7 ">
           <div className="flex justify-between">
             <Animated
               animationIn="fadeInLeft"
@@ -91,7 +106,11 @@ const Home = () => {
           animationInDelay={200}
           animationInDuration={1000}
         >
-          <Image src={AboutImage} width={300} alt="Logo - Nowne" />
+          <Image
+            src={AboutImage}
+            width={280}
+            alt="Mulher falando ao telefone"
+          />
         </Animated>
         <div
           className="
@@ -107,7 +126,7 @@ const Home = () => {
             animationInDelay={200}
             animationInDuration={1000}
           >
-            <p>
+            <p className="text-general">
               Acreditamos na transformação do agora, que combina tecnologia com
               negócios, que otimiza processos e alavanca estratégias para
               alcançar resultados por meio de produtos digitais.
@@ -116,13 +135,18 @@ const Home = () => {
         </div>
       </section>
 
+      {/* linha */}
+      <div className="line line-centered" />
+
       {/* serviços */}
-      <section className="servicesContainer pt-7 pb-7 overflow-hidden">
-        <h3 className="text-2xl sm:text-3xl mt-4 md:text-3xl lg:text-3xl font-semibold leading-1 pl-6 mt-3 mb-7">
-          Serviços que oferecemos
+      <section className="servicesContainer pt-7 pb-10 overflow-hidden">
+        <h3 className="text-2xl sm:text-3xl mt-4 md:text-3xl lg:text-3xl font-bold leading-1 pl-6 pr-6 mt-3 mb-7 text-center">
+          <span style={{ color: '#0ea5e9' }}>Serviços </span>que
+          disponibilizamos para te entregar o{' '}
+          <span style={{ color: '#0ea5e9' }}>melhor</span> produto possível.
         </h3>
 
-        <div className="serviceContainerCard mt-4 mb-5 w-full flex justify-evenly align-center flex-wrap">
+        <div className="serviceContainerCard mt-4 mb-5 w-full flex justify-center align-center flex-wrap">
           <Animated
             animationIn="fadeInDown"
             animationOut="fadeOut"
@@ -130,7 +154,13 @@ const Home = () => {
             animationInDelay={200}
             animationInDuration={1000}
           >
-            <div className="card flex flex-col justify-between">
+            <div className="card flex flex-col">
+              <Image
+                src={serviceImg}
+                width={90}
+                alt="imagem"
+                className="mb-4"
+              />
               <div>
                 <h3
                   className="font-semibold text-2xl sm:text-2xl
@@ -140,13 +170,11 @@ const Home = () => {
                 </h3>
 
                 <p className="mt-5 mb-5">
-                  Consultoria em desenvolvimento de ideias, mapeando oportunidades
-                  de mercado, público alvo, plano de negócios e ideação da
-                  proposta de valor.
+                  Consultoria em desenvolvimento de ideias, mapeando
+                  oportunidades de mercado, público alvo, plano de negócios e
+                  ideação da proposta de valor.
                 </p>
               </div>
-
-              <Image src={serviceImg} width={180} alt="imagem" className="m-auto"/>
             </div>
           </Animated>
 
@@ -157,7 +185,14 @@ const Home = () => {
             animationInDelay={200}
             animationInDuration={1000}
           >
-            <div className="card flex flex-col justify-between">
+            <div className="card flex flex-col">
+              <Image
+                src={serviceImg3}
+                width={90}
+                alt="imagem"
+                className="mb-4"
+              />
+
               <div>
                 <h3
                   className="font-semibold text-2xl sm:text-2xl
@@ -165,13 +200,12 @@ const Home = () => {
                 >
                   Nowne Discovery
                 </h3>
-                <p className="mt-5 mb-5">
+                <p className="mt-5 mb-5 text-general">
                   Consultoria onde mapeamos o produto mínimo viável a ser
                   desenvolvido para que seja possível validar a entrega de valor
                   ao negócio com menor custo e tempo possível.
                 </p>
               </div>
-              <Image src={serviceImg3} width={180} alt="imagem" className="m-auto"/>
             </div>
           </Animated>
 
@@ -182,7 +216,13 @@ const Home = () => {
             animationInDelay={200}
             animationInDuration={1000}
           >
-            <div className="card flex flex-col justify-between">
+            <div className="card flex flex-col">
+              <Image
+                src={serviceImg4}
+                width={90}
+                alt="imagem"
+                className="mb-4"
+              />
               <div>
                 <h3
                   className="font-semibold text-2xl sm:text-2xl
@@ -190,26 +230,100 @@ const Home = () => {
                 >
                   Nowne Launch
                 </h3>
-                <p className="mt-5 mb-5">
+                <p className="mt-5 mb-5 text-general">
                   Serviço de desenvolvimento com time multidiciplinar, visando
                   entrega do escopo solicitado pelo cliente.
                 </p>
               </div>
-
-              <Image
-              src={serviceImg4}
-              width={180}
-              alt="imagem"
-              className="m-auto"
-            />
             </div>
           </Animated>
         </div>
       </section>
-      <section className="pt-7 pb-7">
-        <h3 className="text-2xl sm:text-3xl mt-4 md:text-3xl lg:text-3xl font-semibold leading-1 pl-6 mt-3 mb-7">
-          Tecnologias utilizadas pelo nosso time
+      <section className="tecnologicSection pt-12 pb-10 pl-7 pr-7">
+        <h3 className="text-3xl sm:text-3xl mt-4 md:text-4xl lg:text-4xl font-semibold leading-1 mt-3 mb-7">
+          <span style={{ color: '#f97316' }}>Tecnologias</span> utilizadas pelo
+          nosso <span style={{ color: '#f97316' }}>time</span>
         </h3>
+        <div className="w-full flex flex-wrap lg:justify-start sm:justify-center ">
+          <div className="card-small">
+            <div className="card-hr mr-4">
+              <Image
+                src={ReactIcon}
+                width={50}
+                alt="imagem"
+                className="svg-primary mr-2"
+              />
+            </div>
+            <p className="text-general">REACT</p>
+          </div>
+          <div className="card-small">
+            <div className="card-hr mr-4">
+              <Image
+                src={NodeIcon}
+                width={50}
+                alt="imagem"
+                className="svg-primary mr-2"
+              />
+            </div>
+            <p className="text-general">NODE</p>
+          </div>
+          <div className="card-small">
+            <div className="card-hr mr-4">
+              <Image
+                src={EvernoteIcon}
+                width={50}
+                alt="imagem"
+                className="svg-primary mr-2"
+              />
+            </div>
+            <p className="text-general">POSTGRESQL</p>
+          </div>
+          <div className="card-small">
+            <div className="card-hr mr-4">
+              <Image
+                src={Aws}
+                width={50}
+                alt="imagem"
+                className="svg-primary mr-2"
+              />
+            </div>
+            <p className="text-general">AWS</p>
+          </div>
+          <div className="card-small">
+            <div className="card-hr mr-4">
+              <Image
+                src={Figma}
+                width={50}
+                alt="imagem"
+                className="svg-primary mr-2"
+              />
+            </div>
+            <p className="text-general">FIGMA</p>
+          </div>
+          <div className="card-small">
+            <div className="card-hr mr-4">
+              <Image
+                src={Github}
+                width={50}
+                alt="imagem"
+                className="svg-primary mr-2"
+              />
+            </div>
+            <p className="text-general">GITHUB</p>
+          </div>
+        </div>
+      </section>
+      {/* linha */}
+
+      <div className="line-primary-rotate line-centered" />
+
+
+      <section className="contactContainer">
+        <h3 className="text-3xl sm:text-3xl mt-4 md:text-4xl lg:text-4xl font-semibold leading-1 mt-3 mb-7 text-center">
+          Como podemos ajudar na construção do seu produto?
+        </h3>
+        <button className="contact-btn"><span>Entre em contato conosco</span></button>
+        <p className="text-center">℗ 2023 Nowne - Todos os direitos reservados</p>
       </section>
     </main>
   )
