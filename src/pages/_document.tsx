45a3c902ee51b5240bc912d259c54feea65b6a3d
michelkruger <micheldequadros@gmail.com>
2023-06-16 00:35:22 -0300

    update header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="pt-Br">
       <Head>
-        <title>Nowne</title>
+        {/* <title>Nowne</title> */}
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css"></link>
       </Head>
       <body>
